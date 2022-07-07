7342610287b144be71e3e787a6993f8483b6d11b
Yash Raj <yashraj0207@outlook.com>
2022-07-07 15:36:48 +0530

    P0: Fixed build error

--- src/modules/homepage/homepage.tsx
+++ src/modules/homepage/homepage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import type { NextPage } from 'next'
 
 import { Header } from '@/components/header'
@@ -8,7 +9,6 @@ import { FilterSection } from './sections/filter'
 
 import { useStore } from './homepage.store'
 import { initializePropertyData } from './homepage.utils'
-import { Head } from 'next/document'
 
 const HomePage: NextPage = () => {
     const { appState, setAppState, setInitialData } = useStore((state) => state)
